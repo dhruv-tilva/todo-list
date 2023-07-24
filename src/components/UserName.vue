507b98ad41b40cb1e556bc6fa1828fc54295d319
Dhruv Tilva <tilavadhruv111@gmail.com>
2023-07-24 11:47:07 +0530

    Data has been stored in the Local Storage

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -1,14 +1,14 @@
 <template>
-    <div ref="box" class="max-w-sm bg-slate-200 p-5 mx-auto mb-5">
-        <input @keyup.enter="addItem" ref="inp" v-model="newItem" type="text" placeholder="Add Item"
-            class="px-4 py-[10px] text-lg outline-none">
-        <button v-on:click="addItem" class="px-4 bg-gray-600 py-[10px] ml-2 text-white text-lg">Add</button>
+    <h1 class="mt-40 text-white text-center text-5xl mb-8 font-semibold">Todo App</h1>
+    <div class="max-w-[425px] mx-auto px-4">
+        <input type="text" v-model="items" class="inline-block w-full p-3 outline-none text-[#060921]"
+            placeholder="Add Item" @keyup.enter="addItem">
+        <i class="fa-solid fa-plus ml-[-28px] text-xl text-[#060921] cursor-pointer" @click="addItem"></i>
     </div>
-    <ul class="max-w-sm mx-auto">
-        <li class="" v-for="x in todo">
-            <span class="text-xl font-semibold mr-2">{{ todo.indexOf(x) + 1 }}</span>
-            <span class="text-xl font-semibold">{{ x }}</span>
-            <button v-on:click="removeItem(todo.length - 1)" class="ml-2 text-red-500 text-lg font-medium">X</button>
+    <ul class="max-w-[425px] mx-auto mt-8 px-4">
+        <li class="bg-[#5535be] p-2 flex justify-between items-center mb-2" v-for="x in todos"><span
+                class="text-white text-lg font-semibold">{{ x }}</span> <i
+                class="fa-solid fa-trash text-white text-xl cursor-pointer" @click="removeItem(todos.indexOf(x))"></i>
         </li>
     </ul>
 </template>
@@ -17,30 +17,34 @@
 export default {
     data() {
         return {
-            newItem: "",
-            todo: []
+            todos: [],
+            items: ""
         }
     },
     methods: {
-        addItem: function () {
-            let value = this.newItem.trim();
-            if (value) {
-                this.todo.push(this.newItem);
-                localStorage.setItem(this.todo.indexOf(value) + 1, value)
-                this.newItem = "";
-                this.$refs.inp.classList.remove("border-2");
-                this.$refs.inp.classList.remove("border-red-500");
-                this.$refs.box.classList.remove("bg-red-200");
-            } else {
-                this.$refs.inp.classList.add("border-2");
-                this.$refs.inp.classList.add("border-red-500");
-                this.$refs.box.classList.add("bg-red-200");
+        addItem() {
+            if (this.items.trim() !== "") {
+                this.todos.push(this.items.trim());
+                this.items = "";
+                this.toLocalStorage();
             }
-            console.log(this.todo)
         },
-        removeItem: function (index) {
-            this.todo.splice(index, 1);
+        toLocalStorage() {
+            localStorage.setItem("todo", JSON.stringify(this.todos))
+        },
+        removeItem(index) {
+            this.todos.splice(index, 1);
+            this.toLocalStorage();
+        },
+        getLocalStorage() {
+            const saveData = localStorage.getItem("todo");
+            if (saveData) {
+                this.todos = JSON.parse(saveData)
+            }
         }
+    },
+    mounted() {
+        this.getLocalStorage();
     }
 }
 </script>
