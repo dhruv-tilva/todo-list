c9b11ff618d4b012da8e32b3c07566c492373707
Dhruv Tilva <tilavadhruv111@gmail.com>
2023-08-11 18:31:36 +0530

    First Latter Capitalizes features has been added.

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -2,13 +2,15 @@
     <h1 class="mt-40 text-white text-center text-5xl mb-8 font-semibold">Todo App</h1>
     <div class="max-w-[425px] mx-auto px-4">
         <input type="text" v-model="items" class="inline-block w-full p-3 outline-none text-[#060921]"
-            placeholder="Add Item" @keyup.enter="addItem">
-        <i class="fa-solid fa-plus ml-[-28px] text-xl text-[#060921] cursor-pointer" @click="addItem"></i>
+            placeholder="Add Item" @keyup.enter="addItem" required>
+        <i class="fa-solid fa-plus ml-[-28px] text-xl text-[#060921] opacity-70 cursor-pointer transition-all duration-150 hover:opacity-100"
+            @click="addItem"></i>
     </div>
     <ul class="max-w-[425px] mx-auto mt-8 px-4">
         <li class="bg-[#5535be] p-2 flex justify-between items-center mb-2" v-for="x in todos"><span
                 class="text-white text-lg font-semibold">{{ x }}</span> <i
-                class="fa-solid fa-trash text-white text-xl cursor-pointer" @click="removeItem(todos.indexOf(x))"></i>
+                class="fa-solid fa-trash text-white text-xl cursor-pointer opacity-70 transition-all duration-150 hover:opacity-100"
+                @click="removeItem(todos.indexOf(x))"></i>
         </li>
     </ul>
 </template>
@@ -24,6 +26,7 @@ export default {
     methods: {
         addItem() {
             if (this.items.trim() !== "") {
+                this.items = this.items.charAt(0).toUpperCase() + this.items.slice(1);
                 this.todos.push(this.items.trim());
                 this.items = "";
                 this.toLocalStorage();
